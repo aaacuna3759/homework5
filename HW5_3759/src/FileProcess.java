40f5d75d06a0ed7f20cef17e2c1261e547439c8a
Joe Lodyga <jplodyga@eagle.fgcu.edu>
2015-11-28 19:36:15 -0500

    Added knnCSV function to output data into csv format for matlab, added fullMap function to create map of all words in all files without pre-processing, and added processedMap function to create map of all words in all files with pre-processing. Updated test function to create the knn CSV files.

--- HW5_3759/src/FileProcess.java
+++ HW5_3759/src/FileProcess.java
@@ -2,38 +2,79 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class FileProcess {
 
-	String folderPath;
-	File folder;
-	List<Map<String, Integer>> termVectors = new ArrayList<Map<String, Integer>>();
-	Map<String, Integer> vocabulary = new HashMap<String, Integer>();
-	List<Character> characters = new ArrayList<Character>();
+	String trainFolderPath;
+	String testFolderPath;
+	File trainFolder;
+	File testFolder;
 
-	FileProcess(String folderPath) {
+	public FileProcess(String trainFolderPath, String testFolderPath) {
 
-		this.folderPath = folderPath;
-		this.folder = new File(folderPath);
+		this.trainFolderPath = trainFolderPath;
+		this.trainFolder = new File(trainFolderPath);
+		this.testFolderPath = testFolderPath;
+		this.testFolder = new File(testFolderPath);
 
 	}
 
+	
 	/**
-	 * This function removes all punctuation and symbols from all files in the
-	 * folder
+	 * This function creates a map of all words in all email messages without
+	 * any pre-processing.
 	 * 
-	 * @return true is succesful, false otherwise
+	 * @return HashMap<String, Integer> fullMap
 	 */
-	void preProcess() {
-
-		for (File file : folder.listFiles()) {
-			System.out.println(file);
+	public HashMap<String, Integer> fullMap() {
+		
+		HashMap<String, Integer> fullMap = new HashMap<String, Integer>();
+		for (File file : trainFolder.listFiles()) {
+			try {
+				FileReader fr = new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				String line = br.readLine();
+				while (line != null) {
+					StringTokenizer tokenizer = new StringTokenizer(line);
+					while (tokenizer.hasMoreTokens()) {
+						String word = tokenizer.nextToken();
+						if (fullMap.containsKey(word)) {
+							fullMap.put(word, fullMap.get(word) + 1);
+						} else {
+							fullMap.put(word, 1);
+						}
+					}
+					line = br.readLine();
+				}
+				br.close(); // Close BufferedReader
+			} catch (FileNotFoundException fNFE) {
+				fNFE.printStackTrace();
+			} catch (IOException iOE) {
+				iOE.printStackTrace();
+			}
+		}
+		return fullMap;
+		
+	}
+	
+	
+	/**
+	 * This function removes all punctuation and rare words from all files in the
+	 * folder and creates map of all words and how many times they occur in all files
+	 * for use with task 3 in homework 5.
+	 * 
+	 * @return HashMap<String, Integer> processedMap
+	 */
+	public HashMap<String, Integer> processedMap() {
+		
+		HashMap<String, Integer> processedMap = new HashMap<String, Integer>(); // Map of all words in all emails with processing
+		for (File file : trainFolder.listFiles()) {
 			try {
 				FileReader fr = new FileReader(file);
 				BufferedReader br = new BufferedReader(fr);
@@ -43,33 +84,106 @@ public class FileProcess {
 							" \t\n\r\f,.:;?![]'->@()/+-\"#\\<*_=&~`{}$%|^");
 					while (tokenizer.hasMoreTokens()) {
 						String word = tokenizer.nextToken();
-						if (vocabulary.containsKey(word)) {
-							vocabulary.put(word, vocabulary.get(word) + 1);
-							System.out.println("Updated word: " + word);
+						if (processedMap.containsKey(word)) {
+							processedMap.put(word, processedMap.get(word) + 1);
 						} else {
-							vocabulary.put(word, 1);
-							System.out.println("Added new word: " + word);
-						}
-
-						for (int i = 0; i < word.length(); i++) {
-							if (!characters.contains(word.charAt(i))) {
-								characters.add(word.charAt(i));
-							}
+							processedMap.put(word, 1);
 						}
 					}
 					line = br.readLine();
 				}
+				br.close(); // Close BufferedReader
 			} catch (FileNotFoundException fNFE) {
-				fNFE.printStackTrace();
+					fNFE.printStackTrace();
 			} catch (IOException iOE) {
-				iOE.printStackTrace();
+					iOE.printStackTrace();
 			}
 		}
-
-		/*
-		 * for (char c : characters) { System.out.print(c); }
-		 */
 		
-		System.out.println(vocabulary);
+		// Remove rare words that occur less than 50 times
+		Iterator<HashMap.Entry<String, Integer>> entries = processedMap.entrySet().iterator();
+		while (entries.hasNext()) {
+			  HashMap.Entry<String, Integer> entry = entries.next();
+			  int value = entry.getValue();
+			  if(value < 50) {
+				  entries.remove();
+			  }
+		}
+		
+		return processedMap;
+		
+	}
+
+	
+	/**
+	 * This function outputs a map into CSV format for use in KNN classification.
+	 * 
+	 * @throws IOException 
+	 * @param map is the map of all words and total counts
+	 * @param folderPath is the folder where the messages are located
+	 * @param output is the name of the file that will be created
+	 */
+	public void knnCSV(HashMap<String, Integer> map, String folderPath, String output) throws IOException {
+		
+		FileWriter fw = null;
+		File[] files = null;
+		
+		// Create array of files to use and file to write to
+		if(folderPath.equals("train")) {
+			files = trainFolder.listFiles();
+			fw = new FileWriter("src/" + output, true);
+		}
+		else if(folderPath.equals("test")) {
+			files = testFolder.listFiles();
+			fw = new FileWriter("src/" + output, true);
+		}
+		
+		// Create a temporary map of all words and counts in each individual file
+		for (File file : files) {
+			Map<String, Integer> tempMap = new HashMap<String, Integer>();
+			try {
+				FileReader fr = new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				String line = br.readLine();
+				while (line != null) {
+					StringTokenizer tokenizer = new StringTokenizer(line);
+					while (tokenizer.hasMoreTokens()) {
+						String word = tokenizer.nextToken();
+						if (tempMap.containsKey(word)) {
+							tempMap.put(word, tempMap.get(word) + 1);
+						} else {
+							tempMap.put(word, 1);
+						}
+					}
+					line = br.readLine();
+				}
+				br.close(); // Close BufferedReader
+			} catch (FileNotFoundException fNFE) {
+					fNFE.printStackTrace();
+			} catch (IOException iOE) {
+					iOE.printStackTrace();
+			}
+			
+			// Print value of words in tempMap for each word in fullMap separated by commas
+			Iterator<HashMap.Entry<String, Integer>> entries = map.entrySet().iterator();
+			while (entries.hasNext()) {
+				  HashMap.Entry<String, Integer> entry = entries.next();
+				  String word = entry.getKey();
+				  if(tempMap.containsKey(word)) {
+					  fw.write(tempMap.get(word).toString());
+				  }
+				  else {
+					  fw.write("0");
+				  }
+				  if(entries.hasNext()) {
+					  fw.write(",");
+				  }
+				  else {
+					  fw.write("\r\n");
+				  }
+			}
+		}
+		fw.close();
+		
 	}
 }
--- HW5_3759/src/Test.java
+++ HW5_3759/src/Test.java
@@ -1,9 +1,25 @@
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Test {
 
-	public static void main(String[] args) {
-		FileProcess fileProcess = new FileProcess("src/train");
+	public static void main(String[] args) throws IOException {
+		FileProcess fileProcess = new FileProcess("src/train", "src/test");
+		
+		HashMap<String, Integer> fullMap = new HashMap<String, Integer>();
+		HashMap<String, Integer> processedMap = new HashMap<String, Integer>();
 
-		fileProcess.preProcess();
+		// Output csv files for use in KNN classification
+		fullMap = fileProcess.fullMap(); // Create map of all words from all emails
+		fileProcess.knnCSV(fullMap, "train", "knnTrain.csv"); // Output csv file for training data
+		fileProcess.knnCSV(fullMap, "test", "knnTest.csv"); // Output csv file for test data
+		System.out.println("Data has been formatted into knnTrain.csv and knnTest.csv for KNN classification use in Matlab.");
+		
+		// Output csv files for use in KNN classification
+		processedMap = fileProcess.processedMap(); // Create map of all words from all emails after pre-processing
+		fileProcess.knnCSV(processedMap, "train", "knnTrainPP"); // Output csv file for training data
+		fileProcess.knnCSV(processedMap, "test", "knnTestPP"); // Output csv file for test data
+		System.out.println("Data has been formatted into knnTrainPP.csv and knnTestPP.csv for KNN classification use in Matlab.");
 	}
 
 }
