a100f202b519ba78168ec54bdae44de60a3a1c92
lodygaj <lodygaj@hotmail.com>
2015-12-01 18:55:58 -0500

    Update knnAccuracy.m

--- knnAccuracy.m
+++ knnAccuracy.m
@@ -11,9 +11,9 @@ for i=1:size(testData, 1)
     % calculate the cosine similarity for test point with 
     % each record in trainData and store in similarity vector.
 	for j=1:size(trainData, 1)
-		dotProduct = sum(trainData(j,:) .* testData(i,:));
-		d1 = sqrt( sum(trainData(j,:).^2) );
-		d2 = sqrt( sum(testData(i,:).^2) );
+		dotProduct = sum(trainData(j,1:size(trainData,2)-1).*testData(i,1:size(testData,2)-1));
+		d1 = sqrt( sum(trainData(j,1:size(trainData,2)).^2) );
+		d2 = sqrt( sum(testData(i,1:size(testData,2)).^2) );
 		similarity(j) = dotProduct / (d1 * d2);
     end
         
@@ -25,33 +25,29 @@ for i=1:size(testData, 1)
     kNN_indx = I(1:k); 
 
     % count how many records are regular and how many are spam
-    % indices 1-101 are regular and 102-201 are spam
     for m = 1:k
-        if (kNN_indx(m) <= 101)
+        if (trainData(kNN_indx(m),size(trainData,2)) == 0)
             reg_count = reg_count + 1;
         else 
             spam_count = spam_count + 1;
         end
     end
-
+    
     % determine class of test point: 0 is regular and 1 is spam
     if (reg_count >= spam_count)
         class = 0;
     else
         class = 1;
     end
-
-    % calculate number of correct classifications
-    % indices 1-101 are regular and 102-201 are spam
-    if (class == 0) && (i <= 101)
-        correct = correct + 1;
-    elseif (class == 1) && (i > 101)
+    
+    % determine whether classification was accurate
+    if (testData(i, size(testData,2)) == class)
         correct = correct + 1;
     end
     
 end
 
-% display classification accuracy
+% display total classification accuracy
 accuracy = correct / size(testData, 1)
 
 end
